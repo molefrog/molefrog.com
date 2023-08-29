819bc813291f396c1d4522e648d4fcf117f83bd1
Alexey Taktarov <molefrog@gmail.com>
2023-08-29 18:34:40 +0300

    Rewrites for github hosted projects.

---
+++ functions/etc/[[project]].js
@@ -0,0 +1,33 @@
+const rewrites = {
+  "pid-neural-network": "https://molefrog.github.io/pidnn-talk",
+  "stateful-animations": "https://molefrog.github.io/stateful-animations",
+  "rye-js": "https://molefrog.github.io/rye",
+};
+
+export const onRequest = (context) => {
+  const { project } = context.params;
+  const [name, ...rest] = String(project).split(",");
+
+  if (!(name in rewrites)) {
+    return new Response("(ﾉ´ヮ`)ﾉ*: [404]", { status: 404 });
+  }
+
+  const url = new URL(context.request.url);
+
+  if (rest.length === 0 && !url.pathname.endsWith("/")) {
+    return new Response("Redirecting...", {
+      status: 302,
+      headers: {
+        Location: url.pathname + "/",
+      },
+    });
+  }
+
+  const path = "/" + rest.join("/");
+
+  const newURL = new URL(rewrites[name] + path);
+  newURL.search = url.search;
+
+  const newReq = new Request(String(newURL), context.request);
+  return fetch(newReq);
+};
