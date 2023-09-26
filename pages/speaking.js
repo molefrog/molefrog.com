68180e33176af590d12c6a5e31b1ae91cdd0a525
Alexey Taktarov <molefrog@gmail.com>
2023-09-26 11:46:09 +0200

    Tweaks.

--- pages/speaking.js
+++ pages/speaking.js
@@ -158,7 +158,7 @@ export default function Speaking() {
                 </p>
 
                 <p>
-                  The talk has been well received by the audience and was included in the list of
+                  The talk has been well received by the audience and was included in the list of{" "}
                   <a
                     className="solid-link"
                     href="https://habr.com/ru/companies/jugru/articles/350164/#:~:text=%D0%90%D0%BD%D0%B8%D0%BC%D0%B0%D1%86%D0%B8%D0%B8%20%D0%B2%20%D0%BC%D0%B8%D1%80%D0%B5%20%D1%81%D0%BE%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D0%B9"
