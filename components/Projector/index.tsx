987fba4ff766a4b29a7dfdfbc123f6bca08a429a
Alexey Taktarov <molefrog@gmail.com>
2023-09-30 15:50:17 +0200

    Progress.

--- components/Projector/index.tsx
+++ components/Projector/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image, { StaticImageData } from "next/image";
+import clsx from "clsx";
 
 interface ProjectorProps {
   slides: StaticImageData[];
@@ -7,9 +8,32 @@ interface ProjectorProps {
 }
 
 export const Projector: React.FC<ProjectorProps> = ({ slides, title }) => {
+  const [currentSlide, setCurrentSlide] = useState(1);
+  const slidesRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const el = slidesRef.current;
+    if (!el) return;
+
+    const handleScroll = () => {
+      if (slidesRef.current) {
+        const slideWidth = slidesRef.current.clientWidth;
+        const scrollLeft = slidesRef.current.scrollLeft;
+        const newSlide = Math.ceil(scrollLeft / slideWidth) + 1;
+        setCurrentSlide(newSlide);
+      }
+    };
+
+    el.addEventListener("scroll", handleScroll);
+
+    return () => {
+      el.removeEventListener("scroll", handleScroll);
+    };
+  }, [slides.length]);
+
   return (
     <div className="projector" style={{ "--slides-n": slides.length } as React.CSSProperties}>
-      <div className="projector__slides">
+      <div className="projector__slides" ref={slidesRef}>
         {slides.slice().map((slide, i) => (
           <div className="projector__slide-step" key={slide.src + String(i)}>
             <div className="projector__slide">
@@ -22,6 +46,18 @@ export const Projector: React.FC<ProjectorProps> = ({ slides, title }) => {
               />
             </div>
           </div>
+        ))}
+      </div>
+
+      <div className="projector__progress">
+        {slides.map((_, i) => (
+          <div
+            key={String(i)}
+            className={clsx(
+              "projector__progress-step",
+              currentSlide >= i + 1 ? "projector__progress-step--active" : ""
+            )}
+          />
         ))}
       </div>
     </div>
