36f9e5fc2fa0d30059155d78d4770ae18b733073
Alexey Taktarov <molefrog@gmail.com>
2023-11-15 13:47:46 +0100

    Fix static export.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     // disable Next Image Optimization API for remote images
     unoptimized: true,
