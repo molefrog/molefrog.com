58f1bbae1b1dacca188b711a9036108f639f95ea
Alexey Taktarov <molefrog@gmail.com>
2023-11-18 16:06:41 +0100

    Refactor fonts.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,105 +1,15 @@
 import React from "react";
-import Script from "next/script";
 import { Viewport, type Metadata } from "next";
+import Script from "next/script";
 import { Provider as WrapBalancerProvider } from "react-wrap-balancer";
-import localFont from "next/font/local";
-import { JetBrains_Mono } from "next/font/google";
 
 import TopBar from "@/components/TopBar";
 import Footer from "@/components/Footer";
-
 import favicon from "../public/favicon.svg";
 
-import "../styles/index.css";
+import "@/styles/index.css";
+import { fontsCSSVars } from "@/styles/fonts/fonts";
 import VideoAssets from "@/content/videos";
-
-// If loading a variable font, you don't need to specify the font weight
-const jetBrainsMono = JetBrains_Mono({
-  subsets: ["latin"],
-  display: "swap",
-});
-
-const DSEG7Classic = localFont({
-  src: [
-    {
-      path: "../styles/fonts/DSEG7Classic/DSEG7Classic-Regular.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../styles/fonts/DSEG7Classic/DSEG7Classic-Italic.woff2",
-      weight: "500",
-      style: "italic",
-    },
-    {
-      path: "../styles/fonts/DSEG7Classic/DSEG7Classic-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../styles/fonts/DSEG7Classic/DSEG7Classic-BoldItalic.woff2",
-      weight: "700",
-      style: "italic",
-    },
-  ],
-  fallback: ["monospace"],
-  display: "swap",
-});
-
-const DSEG14Classic = localFont({
-  src: [
-    {
-      path: "../styles/fonts/DSEG14Classic/DSEG14Classic-Regular.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../styles/fonts/DSEG14Classic/DSEG14Classic-Italic.woff2",
-      weight: "500",
-      style: "italic",
-    },
-    {
-      path: "../styles/fonts/DSEG14Classic/DSEG14Classic-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../styles/fonts/DSEG14Classic/DSEG14Classic-BoldItalic.woff2",
-      weight: "700",
-      style: "italic",
-    },
-  ],
-  fallback: ["monospace"],
-  display: "swap",
-});
-
-const Redaction35 = localFont({
-  src: [
-    {
-      path: "../styles/fonts/Redaction/Redaction_35-Regular.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    { path: "../styles/fonts/Redaction/Redaction_35-Italic.woff2", weight: "500", style: "italic" },
-    { path: "../styles/fonts/Redaction/Redaction_35-Bold.woff2", weight: "700", style: "normal" },
-  ],
-  fallback: ["serif"],
-  display: "swap",
-});
-
-const Redaction10 = localFont({
-  src: [
-    {
-      path: "../styles/fonts/Redaction/Redaction_10-Regular.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    { path: "../styles/fonts/Redaction/Redaction_10-Italic.woff2", weight: "500", style: "italic" },
-    { path: "../styles/fonts/Redaction/Redaction_10-Bold.woff2", weight: "700", style: "normal" },
-  ],
-  fallback: ["serif"],
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "Overcoming overcomplication → Alexey Taktarov",
@@ -119,23 +29,8 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html className={fontsCSSVars} lang="en">
       <body>
-        <style
-          dangerouslySetInnerHTML={{
-            __html: `
-          :root {
-            --font-sans: system-ui, sans-serif;
-            --font-serif: ${Redaction35.style.fontFamily};
-            --font-serif-fine: ${Redaction10.style.fontFamily};
-            --font-mono: ${jetBrainsMono.style.fontFamily};
-            --font-segm7: ${DSEG7Classic.style.fontFamily};
-            --font-segm14: ${DSEG14Classic.style.fontFamily};
-          }
-        `,
-          }}
-        />
-
         <VideoAssets />
 
         <WrapBalancerProvider>
---
+++ styles/fonts/fonts.ts
@@ -0,0 +1,79 @@
+import localFont from "next/font/local";
+import { JetBrains_Mono } from "next/font/google";
+
+const JetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["monospace"],
+  variable: "--font-mono",
+});
+
+const DSEG7Classic = localFont({
+  src: [
+    {
+      path: "./DSEG7Classic/DSEG7Classic-Bold.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./DSEG7Classic/DSEG7Classic-BoldItalic.woff2",
+      weight: "500",
+      style: "italic",
+    },
+  ],
+  fallback: ["monospace"],
+  variable: "--font-segm7",
+  display: "swap",
+});
+
+const DSEG14Classic = localFont({
+  src: [
+    {
+      path: "./DSEG14Classic/DSEG14Classic-Bold.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./DSEG14Classic/DSEG14Classic-BoldItalic.woff2",
+      weight: "500",
+      style: "italic",
+    },
+  ],
+  fallback: ["monospace"],
+  variable: "--font-segm14",
+  display: "swap",
+});
+
+const Redaction35 = localFont({
+  src: [
+    {
+      path: "./Redaction/Redaction_35-Regular.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    { path: "./Redaction/Redaction_35-Italic.woff2", weight: "500", style: "italic" },
+  ],
+  fallback: ["serif"],
+  variable: "--font-serif",
+  display: "swap",
+});
+
+const Redaction10 = localFont({
+  src: [
+    {
+      path: "./Redaction/Redaction_10-Regular.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    { path: "./Redaction/Redaction_10-Italic.woff2", weight: "500", style: "italic" },
+  ],
+  fallback: ["serif"],
+  variable: "--font-serif-fine",
+  display: "swap",
+});
+
+export const fontsCSSVars = [JetBrainsMono, DSEG7Classic, DSEG14Classic, Redaction35, Redaction10]
+  .map((f) => f.variable)
+  .join(" ");
+
+export { JetBrainsMono, DSEG7Classic, DSEG14Classic, Redaction35, Redaction10 };
