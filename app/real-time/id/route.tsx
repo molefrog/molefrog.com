4ea3275d2051575d1ae759725c758d7736e4bdfc
Alexey Taktarov <molefrog@gmail.com>
2024-11-26 18:31:05 +0900

    Fix route.

--- app/real-time/id/route.tsx
+++ app/real-time/id/route.tsx
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-static";
+
 // it's a static mock endpoint that is only used in development
 // see functions/real-time/id.js for real implementation
 export async function GET() {
